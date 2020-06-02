15b686c25c929247464154c2e5bfb10cdeddfcaf
Waldir Fortes <waldirfortes@enacol.local>
2020-06-02 17:40:20 -0100

    Library OK

--- _app/Email.php
+++ _app/Email.php
@@ -9,25 +9,25 @@ class Email
 {
     private $mail = \stdClass::class;
 
-    public function __construct()
+    public function __construct($smtpDebug, $host, $user, $pass, $smtpSecure, $port, $setFromEmail, $setFromName)
     {
         $this->mail = new PHPMailer(true);
 
         //Server settings
-        $this->mail->SMTPDebug = 2;                           // Enable verbose debug output
-        $this->mail->isSMTP();                                // Send using SMTP
-        $this->mail->Host       = 'smtp.gmail.com';           // Set the SMTP server to send through
-        $this->mail->SMTPAuth   = 'false';                       // Enable SMTP authentication
-        $this->mail->Username   = 'waldirfortes@gmail.com';   // SMTP username
-        $this->mail->Password   = 'W@ld1r1978F0rt3s';         // SMTP password
-        $this->mail->SMTPSecure = 'ssl';                      // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
-        $this->mail->Port       = 465;                        // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+        $this->mail->SMTPDebug = $smtpDebug;       // Enable verbose debug output
+        $this->mail->isSMTP();                     // Send using SMTP
+        $this->mail->Host       = $host;           // Set the SMTP server to send through
+        $this->mail->SMTPAuth   = 'false';         // Enable SMTP authentication
+        $this->mail->Username   = $user;           // SMTP username
+        $this->mail->Password   = $pass;           // SMTP password
+        $this->mail->SMTPSecure = $smtpSecure;     // (ssl) Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+        $this->mail->Port       = $port;           // (465) TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
         $this->mail->CharSet    = 'utf-8';
         $this->mail->setLanguage('pt');
         $this->mail->isHTML(true);
 
         //Recipients
-        $this->mail->setFrom('waldirfortes@gmail.com', 'Developer nhôWaldir');
+        $this->mail->setFrom($setFromEmail, $setFromName);
     }
 
     public function sendMail($subject, $body, $replayEmail, $replayName, $addressEmail, $addressName)
--- test/index.php
+++ test/index.php
@@ -1,10 +1,19 @@
 <?php
 
-require __DIR__. '/lib_ext/autoload.php';
+require __DIR__. '/../lib_ext/autoload.php';
 
 use Notification\Email;
 
-$nomeEmail = new Email;
+$nomeEmail = new Email(
+    2,
+    "smtp.gmail.com",
+    "waldirfortes@gmail.com",
+    "W@ld1r1978F0rt3s",
+    "ssl",
+    465,
+    "waldirfortes@gmail.com",
+    "Equipa WFSoluction" );
+
 $nomeEmail->sendMail(
     "Assunto de Teste",
     "<p>Esse é um e-mail de <b>teste II - Nhô Waldir</b>!</p>",
