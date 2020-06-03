68817ecf232c448445e5775eebfb51ed2691885d
Waldir Fortes <waldirfortes@enacol.local>
2020-06-03 13:03:07 -0100

    Test fix

--- test/index.php
+++ test/index.php
@@ -4,20 +4,6 @@ require __DIR__. '/../lib_ext/autoload.php';
 
 use Notification\Email;
 
-$novoEmail = new Email(
-    2,
-    "smtp.gmail.com",
-    "waldirfortestest@gmail.com",
-    "test@123",
-    "ssl",
-    465,
-    "waldirfortestest@gmail.com",
-    "Equipa WFSoluction" );
+$email = new Email(2, "mail.host.com", "your@email.com", "your-pass", "secure smtp (tls / ssl)", "port (587)","from@email.com", "From name");
 
-$novoEmail->sendMail(
-    "Assunto de Teste",
-    "<p>Esse é um e-mail de <b>teste II - Nhô Waldir</b>!</p>",
-    "waldirfortestest@gmail.com",
-    "Waldir Developer",
-    "waldirfortestest@gmail.com",
-    "Waldir Enacol");
+$email->sendEmail("Subject", "Content", "reply@email.com", "Replay Name", "address@email.com", "Address Name");
